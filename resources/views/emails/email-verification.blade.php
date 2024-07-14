3d642dbcb99a2e11ae8009ce382e7598651de589
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-14 23:07:28 +0300

    Implementing the email-verification.blade.php file and setup the email layout

--- resources/views/emails/email-verification.blade.php
+++ resources/views/emails/email-verification.blade.php
@@ -3,10 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Task Management System</title>
 </head>
 <body>
+    <div class="container" >
+        <div class="card card-body" style="margin-left: 40px;margin-left: 50px;">
+            <div align="center">
+                <a href="{{url('/')}}">
+                    <img width="100" src="{{url('/others/logo.png')}}">
+                </a>
+            </div>
+            <h3>E-mail verification.</h3>
+            <p>Hello, <br/><br/>
+                we have sent to you this email to check if this Email : <a href="#">{{$user->email}}</a> you provide is a valid one; click on the link to verifiy it.
 
+                <a style="font-weight:bold;color:blue" target="_blank" href="{{env('APP_URL')}}/check_email/{{$user->remember_token}}">
+                    Check my email
+                </a>
+            </p>
+            <br/>
+            <p>Yours sincerely.</p>
+        </div>
+    </div>
 </body>
 </html>
