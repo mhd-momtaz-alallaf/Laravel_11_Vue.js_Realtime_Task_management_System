53c698a7df40b388eea4a089f9a40ca684d8e248
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-14 23:06:20 +0300

    Implementing the SendEmailVerification Listener to send an email for each registered user

--- app/Listeners/SendEmailVerification.php
+++ app/Listeners/SendEmailVerification.php
@@ -2,8 +2,11 @@
 
 namespace App\Listeners;
 
+use App\Events\NewUserCreated;
+use App\Mail\SendVerificationMail;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
 
 class SendEmailVerification
 {
@@ -18,8 +21,10 @@ class SendEmailVerification
     /**
      * Handle the event.
      */
-    public function handle(object $event): void
+
+    // Listening for NewUserCreated event, the listener will get the user instance from the event, then will send the email to the created user.
+    public function handle(NewUserCreated $event): void
     {
-        //
+        Mail::to($event->user->email)->send(new SendVerificationMail($event->user));
     }
 }
