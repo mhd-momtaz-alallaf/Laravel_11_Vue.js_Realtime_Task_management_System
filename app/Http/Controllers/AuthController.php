af9643dd5ffe5399cefe5e281ad87bdfde2028db
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-14 18:24:20 +0300

    implementing the Register function in the AuthController.php file

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $user_fields = $request->all();
+
+        $errors = Validator::make($user_fields, [
+            'email' => 'required',
+            'password' => 'required|min:6|max:8',
+        ]);
+
+        if($errors->fails()){
+            return response($errors->errors()->all(), 422);
+        }
+
+        User::create([
+            'email' => $user_fields['email'],
+            'password' => bcrypt($user_fields['password']),
+            'isValidEmail' => User::IS_INVALID_EMAIL,
+            'remember_token' => $this->generateRandomString(),
+        ]);
+
+        return response(['message' => 'User Created Successfully!'], 200);
+    }
+
+    // generating a random string for use it as the user remember token.
+    function generateRandomString()
+    {
+        return Str::random(10) . time();
+    }
 }
