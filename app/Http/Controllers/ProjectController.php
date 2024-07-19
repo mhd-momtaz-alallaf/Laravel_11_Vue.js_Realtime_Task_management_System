fcce3d25eeca5dbc6b1582517e594e8b26280c19
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-19 22:57:33 +0300

    Creating and Implementing the update method in the ProjectController.php file

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
-use App\Http\Requests\ProjectStoreRequest;
+use App\Http\Requests\ProjectRequest;
 use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function store(ProjectStoreRequest $request)
+    // Create new project.
+    public function store(ProjectRequest $request)
     {
         $validated = $request->validated();
 
@@ -23,6 +24,23 @@ class ProjectController extends Controller
 
         return response([
             'message' => 'Project created successfully!',
+        ], 200);
+    }
+
+    // update a project.
+    public function update(ProjectRequest $request, Project $project)
+    {
+        $validated = $request->validated();
+
+        $project->update([
+            'name' => $validated['name'],
+            'startDate' => $validated['startDate'],
+            'endDate' => $validated['endDate'],
+            'slug' => Project::generateSlug($validated['name']),
+        ]);
+
+        return response([
+            'message' => 'Project updated successfully!',
         ], 200);
     }
 }
