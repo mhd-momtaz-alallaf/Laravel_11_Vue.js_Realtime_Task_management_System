6709215237ac092c3a74f1df1f05bb55c2ea9f4b
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-19 18:23:32 +0300

    Implementing the projects migration

--- database/migrations/2024_07_19_150336_create_projects_table.php
+++ database/migrations/2024_07_19_150336_create_projects_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->integer('status');
+            $table->date('startDate');
+            $table->date('endDate');
             $table->timestamps();
         });
     }
