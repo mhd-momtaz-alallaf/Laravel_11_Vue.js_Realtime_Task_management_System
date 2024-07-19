5558413e174a8df66d66d32802636df113202b4c
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-19 22:51:06 +0300

    Renaming the ProjectStoreRequest to ProjectRequest

--- app/Http/Requests/ProjectRequest.php
+++ app/Http/Requests/ProjectRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ProjectStoreRequest extends BaseRequest
+class ProjectRequest extends BaseRequest
 {
     /**
      * Determine if the user is authorized to make this request.
