b1d15edfa9b9b68e6e03707828f43b5ac9e6d5c8
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-19 18:25:06 +0300

    Implementing the Project Model fillable and the const properties

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'status',
+        'startDate',
+        'endDate',
+    ];
+
+    const NOT_STARTED = 0;
+    const PENDING = 1;
+    const COMPLETED = 2;
 }
