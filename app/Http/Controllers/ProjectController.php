4d5e333abb6cff18396c3ff12a3709d9d1263474
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-19 20:24:51 +0300

    Implementing the store method in the ProjectController.php file to create a new project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\ProjectStoreRequest;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function store(ProjectStoreRequest $request)
+    {
+        $validated = $request->validated();
+
+        $project = Project::create([
+            'name' => $validated['name'],
+            'startDate' => $validated['startDate'],
+            'endDate' => $validated['endDate'],
+            'status' => Project::NOT_STARTED,
+            'slug' => Project::generateSlug($validated['name']),
+        ]);
+
+        return response([
+            'message' => 'Project created successfully!',
+        ], 200);
+    }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Number;
+use Illuminate\Support\Str;
 
 class Project extends Model
 {
@@ -20,4 +22,9 @@ class Project extends Model
     const NOT_STARTED = 0;
     const PENDING = 1;
     const COMPLETED = 2;
+
+    // a static function to create a slug for the project name.
+    static function generateSlug($projectName){
+        return Str::slug($projectName) . '-' . Str::lower(Str::random(4));
+    }
 }
