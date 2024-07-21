8f3e33fabf21c93b9373e4a5f16063efb25485af
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-21 21:20:06 +0300

    Wrapping the creation statements of the store method with a DB:transaction in the ProjectController.php file

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\ProjectRequest;
 use App\Models\Project;
 use App\Models\TaskProgress;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProjectController extends Controller
 {
@@ -21,27 +22,30 @@ class ProjectController extends Controller
     // Create new project.
     public function store(ProjectRequest $request)
     {
-        $validated = $request->validated();
+        // wrapping the creation statements with a transaction.
+        return DB::transaction(function() use ($request){
+            $validated = $request->validated();
 
-        // storing the project in the database.
-        $project = Project::create([
-            'name' => $validated['name'],
-            'startDate' => $validated['startDate'],
-            'endDate' => $validated['endDate'],
-            'status' => Project::NOT_STARTED,
-            'slug' => Project::generateSlug($validated['name']),
-        ]);
+            // storing the project in the database.
+            $project = Project::create([
+                'name' => $validated['name'],
+                'startDate' => $validated['startDate'],
+                'endDate' => $validated['endDate'],
+                'status' => Project::NOT_STARTED,
+                'slug' => Project::generateSlug($validated['name']),
+            ]);
 
-        // adding the initial project progress value (associating the Project Model with the TaskProgress Model).
-        TaskProgress::create([
-            'project_id' => $project->id,
-            'pinned_on_dashboard' => TaskProgress::NOT_PINNED_ON_DASHBOARD,
-            'progress' => TaskProgress::INITIAL_PROJECT_PROGRESS_PERCENT,
-        ]);
+            // adding the initial project progress value (associating the Project Model with the TaskProgress Model).
+            TaskProgress::create([
+                'project_id' => $project->id,
+                'pinned_on_dashboard' => TaskProgress::NOT_PINNED_ON_DASHBOARD,
+                'progress' => TaskProgress::INITIAL_PROJECT_PROGRESS_PERCENT,
+            ]);
 
-        return response([
-            'message' => 'Project created successfully!',
-        ], 200);
+            return response([
+                'message' => 'Project created successfully!',
+            ], 200);
+        });
     }
 
     // update a project.
