4b3df65d8b7c6e164f0dce174c6c11db43b31add
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-21 21:18:08 +0300

    Adding the fillable mass assignment fields and a const variable to the TaskProgress Model

--- app/Models/TaskProgress.php
+++ app/Models/TaskProgress.php
@@ -9,6 +9,13 @@ class TaskProgress extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'project_id',
+        'pinned_on_dashboard',
+        'progress',
+    ];
+
     const NOT_PINNED_ON_DASHBOARD = 0;
     const PINNED_ON_DASHBOARD = 1;
+    const INITIAL_PROJECT_PROGRESS_PERCENT = 0;
 }
