99da8319b0bbf3f2bc51480127f75bfea7664de3
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-21 23:50:37 +0300

    Creating and Implementing the index method of the MemberController.php file

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MemberController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        // getting the search parameter that passed with the route.
+        $query = $request->get('search');
+
+        // $members = Member::select('name','email'); // the same of bellow.
+        $members = DB::table('members');
+
+        // getting only the members having the same name of the search parameter (if the query exist).
+        if ($query) {
+            $members->where('name', 'like', "%{$query}%");
+        }
+
+        $members->orderBy('id', 'desc');
+
+        return response()->json(['data' => $members->paginate(10)], 200);
+    }
 }
