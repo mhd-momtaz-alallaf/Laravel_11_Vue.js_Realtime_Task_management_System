5f40981b662236195b8ec4bff046bc758393eccd
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-23 10:36:59 +0300

    Implementing the tasks migration

--- database/migrations/2024_07_23_072416_create_tasks_table.php
+++ database/migrations/2024_07_23_072416_create_tasks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('project_id')->constrained('projects');
+            $table->string('name');
+            $table->integer('status');
             $table->timestamps();
         });
     }
