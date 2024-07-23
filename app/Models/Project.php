cdf12676a08096a56c492e05fbf716b6f80e8f9e
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-23 12:29:57 +0300

    Creating the relation between the project and the task model in the Project.php file

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Number;
 use Illuminate\Support\Str;
@@ -24,14 +25,21 @@ class Project extends Model
     const PENDING = 1;
     const COMPLETED = 2;
 
-    // a static function to create a slug for the project name.
-    static function generateSlug($projectName){
-        return Str::slug($projectName) . '-' . Str::lower(Str::random(4));
-    }
-
-    // the relation between the project and the task_progress.
+    // the relation between the project and the task_progress model.
     public function task_progress(): HasOne
     {
         return $this->hasOne(TaskProgress::class); // each project => one task_progress.
+    }
+
+    // the relation between the project and the task model.
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class); // each project => many tasks.
+    }
+
+    // a static function to create a slug for the project name.
+    static function generateSlug($projectName)
+    {
+        return Str::slug($projectName) . '-' . Str::lower(Str::random(4));
     }
 }
