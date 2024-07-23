3a5a47b3e2b0ebc662446421b88d7ae67f82c71b
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-23 17:53:06 +0300

    Creating the projects count endpoint in the api.php file

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::controller(AuthController::class)->group(function(){
+Route::controller(AuthController::class)->group(function() {
     // Register Post route.
     Route::post('/register', 'register');
 
@@ -19,12 +19,16 @@ Route::controller(AuthController::class)->group(function(){
     Route::post('/login', 'login');
 });
 
-Route::controller(ProjectController::class)->group(function(){
+Route::controller(ProjectController::class)->group(function() {
     // Storing Project post route.
     Route::post('/projects', 'store');
 
+    // projects count endpoint.
+    Route::get('/projects/count', 'projectsCount');
+
     // Updating Project put route.
-    Route::put('/projects/{project}', 'update');
+    Route::put('/projects/{project}', 'update')
+        ->where('project', '[0-9]+');
 
     // Getting all the projects route.
     Route::get('/projects', 'index');
@@ -33,10 +37,11 @@ Route::controller(ProjectController::class)->group(function(){
     Route::get('/projects/{project:slug}', 'show');
 
     // Pinning a project on dashboard route.
-    Route::post('/projects/{project}/pin-on-dashboard', 'pinProjectOnDashboard');
+    Route::post('/projects/{project}/pin-on-dashboard', 'pinProjectOnDashboard')
+        ->where('project', '[0-9]+');
 });
 
-Route::controller(MemberController::class)->group(function(){
+Route::controller(MemberController::class)->group(function() {
     // Getting all the members.
     Route::get('/members', 'index');
 
@@ -44,10 +49,11 @@ Route::controller(MemberController::class)->group(function(){
     Route::post('/members', 'store');
 
     // Updating a member.
-    Route::put('/members/{member}', 'update');
+    Route::put('/members/{member}', 'update')
+        ->where('member', '[a-zA-Z0-9]+');
 });
 
-Route::controller(TaskController::class)->group(function(){
+Route::controller(TaskController::class)->group(function() {
     // storing a new task.
     Route::post('/tasks', 'store');
 });
