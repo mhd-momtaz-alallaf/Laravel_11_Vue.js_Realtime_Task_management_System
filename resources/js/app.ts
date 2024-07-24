57f93b3b592a6a0bb7accf73dbfe94a2ff479c45
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-24 15:51:13 +0300

    Importing vue-toast-notification to the app.ts file and make a helper function to use it easily when needed

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -3,7 +3,12 @@ import './bootstrap';
 import {createApp} from 'vue'
 import App from './/src/App.vue'
 import router from './src/router/index';
+import ToastPlugin from 'vue-toast-notification';
+// Import one of the available themes
+//import 'vue-toast-notification/dist/theme-default.css';
+import 'vue-toast-notification/dist/theme-bootstrap.css';
 
 createApp(App)
 .use(router)
+.use(ToastPlugin)
 .mount("#app")
