f0490c8a46437bef63c6e47fa503f993cb83d468
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-26 19:48:29 +0300

    Creating the base of MemberPage.vue file and using the MemberTable.vue component

--- resources/js/src/pages/admin/member/MemberPage.vue
+++ resources/js/src/pages/admin/member/MemberPage.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import MemberTable from './components/MemberTable.vue';
+</script>
+
 <template>
-    <h1>Member Page</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header">
+                        Members
+                        <router-link style="float:right" to="/create-members" class="btn btn-primary">Create Member</router-link>
+                    </div>
+
+                    <div class="card-body">
+                        <MemberTable />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
