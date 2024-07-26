c0ae58359bca89704ef313d877d66b3246b0b241
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-26 19:46:02 +0300

    Creating MemberTable.vue Component the Base layout table of Members

---
+++ resources/js/src/pages/admin/member/components/MemberTable.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <table class="table table-bordered table-hove">
+        <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Email</td>
+        </tr>
+
+        <tr>
+            <td>#1</td>
+            <td>Name</td>
+            <td>Email</td>
+        </tr>
+    </table>
+</template>
