f56895cc9e01a4ea604feb23646aff90aa6041d1
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-27 01:42:39 +0300

    Modifying the declaration file and adding PaginationLink and PaginationData objects

--- resources/js/shims-laravel-vue-pagination.d.ts
+++ resources/js/shims-laravel-vue-pagination.d.ts
@@ -1,5 +1,26 @@
-// this is a declaration file for laravel-vue-pagination to able to import it in the app.ts file
+// // this is a declaration file for laravel-vue-pagination to able to import it in the app.ts file
 declare module 'laravel-vue-pagination' {
     import { DefineComponent } from 'vue';
-    export const Bootstrap5Pagination: DefineComponent<{}, {}, any>;
+
+    // Define the structure of pagination data based on the API response
+    interface PaginationLink {
+        url: string | null;
+        label: string;
+        active: boolean;
+    }
+
+    interface PaginationData {
+        current_page: number;
+        last_page: number;
+        links: PaginationLink[];
+        per_page?: number;
+        total?: number;
+    }
+
+    // Define the props and events for the Bootstrap5Pagination component
+    export const Bootstrap5Pagination: DefineComponent<{
+        data: PaginationData;
+    }, {}, {
+        'pagination-change-page': (page: number) => void;
+    }>;
 }
