feb3e2cef19394533f4bbc037df0bd4ef949f4f7
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-26 21:23:16 +0300

    Modifying the MemberTable.vue component and Listing the real members data in the table

--- resources/js/src/pages/admin/member/components/MemberTable.vue
+++ resources/js/src/pages/admin/member/components/MemberTable.vue
@@ -1,18 +1,39 @@
 <script setup lang="ts">
+import { GetMemberType, MemberType } from '../actions/getMember';
+
+defineProps<{
+    members: GetMemberType;
+}>();
+
+const emit = defineEmits<{
+    (e: "editMember", member: MemberType): void;
+}>();
 </script>
 
 <template>
-    <table class="table table-bordered table-hove">
-        <tr>
-            <td>Id</td>
-            <td>Name</td>
-            <td>Email</td>
-        </tr>
-
-        <tr>
-            <td>#1</td>
-            <td>Name</td>
-            <td>Email</td>
-        </tr>
+    <table class="table table-bordered table-hover table-striped">
+        <thead>
+            <tr>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Edit</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="member in members?.data?.data" :key="member.id">
+                <td>{{ member.id }}</td>
+                <td>{{ member.name }}</td>
+                <td>{{ member.email }}</td>
+                <td>
+                    <button
+                        @click="emit('editMember', member)"
+                        class="btn btn-outline-primary"
+                    >
+                        Edit
+                    </button>
+                </td>
+            </tr>
+        </tbody>
     </table>
 </template>
