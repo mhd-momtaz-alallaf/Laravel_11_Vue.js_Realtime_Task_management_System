1fdfae47ce6c4a500c0598dd6acfc7474c1c1325
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-27 23:52:52 +0300

    Creating the logoutUser method in the AuthController.php file

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,20 +14,14 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $user_fields = $request->all();
-
-        $errors = Validator::make($user_fields, [
+        $validated = $request->validate([
             'email' => 'required|email|unique:users,email',
             'password' => 'required|min:6|max:8',
         ]);
 
-        if($errors->fails()){
-            return response($errors->errors()->all(), 422);
-        }
-
         $user = User::create([
-            'email' => $user_fields['email'],
-            'password' => bcrypt($user_fields['password']),
+            'email' => $validated['email'],
+            'password' => bcrypt($validated['password']),
             'isValidEmail' => User::IS_INVALID_EMAIL,
             'remember_token' => $this->generateRandomString(),
         ]);
@@ -35,7 +29,7 @@ class AuthController extends Controller
         // sending an email immediately to the user after creating a user event.
         NewUserCreated::dispatch($user);
 
-        return response([
+        return response()->json([
             'message' => 'User Created Successfully!',
             'user' => $user,
         ], 200);
@@ -53,6 +47,13 @@ class AuthController extends Controller
         // searching for the user email in the database.
         $user = User::where('email', $validated['email'])->first();
 
+        if(!$user || !Hash::check($validated['password'], $user->password)){
+            return response([
+                'message' => 'email or password are invalid!',
+                'isLoggedIn' => false,
+            ], 422);
+        }
+
         if($user){
             if($user->isValidEmail !== User::IS_VALID_EMAIL){
                 NewUserCreated::dispatch($user);
@@ -64,18 +65,24 @@ class AuthController extends Controller
             }
         }
 
-        if(!$user || !Hash::check($validated['password'], $user->password)){
-            return response([
-                'message' => 'email or password are invalid!',
-                'isLoggedIn' => false,
-            ], 422);
-        }
 
-        return response([
+        return response()->json([
             'message' => 'You are logged in!',
             'isLoggedIn' => true,
             'user' => $user,
             'token' => $user->createToken($this->secretKey)->plainTextToken,
+        ], 200);
+    }
+
+    // Logout a user (Deleting his token from the database).
+    public function logoutUser(Request $request){
+
+        DB::table('personal_access_tokens')
+        ->where('tokenable_id', $request->user()->id)
+        ->delete();
+
+        return response()->json([
+            'message' => 'You Are Logged out!'
         ], 200);
     }
 
