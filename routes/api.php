58b2d3aa2e40d4b160ce2dc9ace91e1aaf41c51f
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-27 16:46:58 +0300

    Protecting the api endpoints with 'auth:sanctum' middleware and Modifying tha Bootstrap/app.php file to handel the unauthenticated exceptions

--- routes/api.php
+++ routes/api.php
@@ -19,50 +19,52 @@ Route::controller(AuthController::class)->group(function() {
     Route::post('/login', 'login');
 });
 
-Route::controller(ProjectController::class)->group(function() {
-    // Storing Project post route.
-    Route::post('/projects', 'store');
+Route::middleware('auth:sanctum')->group(function() {
+    Route::controller(ProjectController::class)->group(function() {
+        // Storing Project post route.
+        Route::post('/projects', 'store');
 
-    // projects count endpoint.
-    Route::get('/projects/count', 'projectsCount');
+        // projects count endpoint.
+        Route::get('/projects/count', 'projectsCount');
 
-    // Updating Project put route.
-    Route::put('/projects/{project}', 'update')
-        ->where('project', '[0-9]+');
+        // Updating Project put route.
+        Route::put('/projects/{project}', 'update')
+            ->where('project', '[0-9]+');
 
-    // Getting all the projects route.
-    Route::get('/projects', 'index');
+        // Getting all the projects route.
+        Route::get('/projects', 'index');
 
-    // Show a project details route.
-    Route::get('/projects/{project:slug}', 'show');
+        // Show a project details route.
+        Route::get('/projects/{project:slug}', 'show');
 
-    // Pinning a project on dashboard route.
-    Route::post('/projects/{project}/pin-on-dashboard', 'pinProjectOnDashboard')
-        ->where('project', '[0-9]+');
-});
+        // Pinning a project on dashboard route.
+        Route::post('/projects/{project}/pin-on-dashboard', 'pinProjectOnDashboard')
+            ->where('project', '[0-9]+');
+    });
 
-Route::controller(MemberController::class)->group(function() {
-    // Getting all the members.
-    Route::get('/members', 'index');
+    Route::controller(MemberController::class)->group(function() {
+        // Getting all the members.
+        Route::get('/members', 'index');
 
-    // storing a new member.
-    Route::post('/members', 'store');
+        // storing a new member.
+        Route::post('/members', 'store');
 
-    // Updating a member.
-    Route::put('/members/{member}', 'update')
-        ->where('member', '[a-zA-Z0-9]+');
-});
+        // Updating a member.
+        Route::put('/members/{member}', 'update')
+            ->where('member', '[a-zA-Z0-9]+');
+    });
 
-Route::controller(TaskController::class)->group(function() {
-    // storing a new task.
-    Route::post('/tasks', 'store');
+    Route::controller(TaskController::class)->group(function() {
+        // storing a new task.
+        Route::post('/tasks', 'store');
 
-    // updating the task status to pending endpoint.
-    Route::put('/tasks/change-status-to-pending', 'updateTaskStatusToPending');
+        // updating the task status to pending endpoint.
+        Route::put('/tasks/change-status-to-pending', 'updateTaskStatusToPending');
 
-    // updating the task status to completed endpoint.
-    Route::put('/tasks/change-status-to-completed', 'updateTaskStatusToCompleted');
+        // updating the task status to completed endpoint.
+        Route::put('/tasks/change-status-to-completed', 'updateTaskStatusToCompleted');
 
-    // updating the task status to not started endpoint.
-    Route::put('/tasks/change-status-to-not-started', 'updateTaskStatusToNotStarted');
+        // updating the task status to not started endpoint.
+        Route::put('/tasks/change-status-to-not-started', 'updateTaskStatusToNotStarted');
+    });
 });
