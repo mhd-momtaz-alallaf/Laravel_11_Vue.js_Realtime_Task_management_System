3a29ca0c53251c7dabaf4ee4b83593a74fbc5fc3
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-28 18:02:39 +0300

    Creating the Base Layout of the Dashboard and testing the ApexDonut.vue and ApexRadialBar.vue Components in the DashboardPage.vue file

--- resources/js/src/pages/admin/dashboard/DashboardPage.vue
+++ resources/js/src/pages/admin/dashboard/DashboardPage.vue
@@ -1,3 +1,61 @@
+<script setup lang="ts">
+import ApexDonut from './/components/ApexDonut.vue';
+import ApexRadialBar from './/components/ApexRadialBar.vue';
+</script>
+
 <template>
-    <h1>Dashboard</h1>
+    <div class="row">
+        <h2>Dashboard</h2>
+        <br />
+        <br />
+        <br />
+        <div class="row">
+            <div class="col-md-8">
+                <h3 style="color: rgb(118, 119, 120)">
+                    Project:
+                </h3>
+            </div>
+        </div>
+        <br /><br />
+        <div class="row">
+            <div class="col-md-4 col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        <b>Total Projects</b>
+                    </div>
+                    <div class="card-body">
+                        <br />
+                        <br />
+                        <h2 align="center">11</h2>
+                        <br />
+                        <br />
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <div class="card">
+                    <div class="card-header"><b>Tasks</b></div>
+                    <div class="card-body">
+                        <div>
+                            <ApexDonut :task="[40,60]" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        <b>Task Progress</b>
+                    </div>
+
+                    <div class="card-body">
+                        <div>
+                            <ApexRadialBar :percent="70"/>
+                        </div>
+                        <br />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
