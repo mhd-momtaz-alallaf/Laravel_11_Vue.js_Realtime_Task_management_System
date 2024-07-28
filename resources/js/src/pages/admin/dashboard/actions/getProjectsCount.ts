53d0410ad199e56274984d655f6fe0120e8c8180
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-29 00:40:36 +0300

    Modifying the getProjectsCount.ts action and using the laravel reverb to update the projects count in realtime with each time a new project create

--- resources/js/src/pages/admin/dashboard/actions/getProjectsCount.ts
+++ resources/js/src/pages/admin/dashboard/actions/getProjectsCount.ts
@@ -15,9 +15,27 @@ export function useGetTotalProjects() {
                 "GET"
             );
             projects.value = data;
+
+            // Ensure that the event listener is set up only once
+            if (!window.Echo.joined) {
+                updateData();
+                window.Echo.joined = true;
+            }
         } catch (error) {
             showErrorResponse(error);
         }
+    };
+
+    // using laravel reverb to update the projects count in realtime with each time a new project is created.
+    function updateData() {
+        // listening to 'countProjectsChannel' of 'NewProjectCreated' Event.
+        window.Echo.channel("countProjectsChannel").listen("NewProjectCreated", (e: { projectsCount: number }) => {
+                projects.value = {
+                    projects_count: e.projectsCount,
+                };
+                //console.log(e);
+            },
+        );
     };
 
     return { projects, getTotalProjects };
