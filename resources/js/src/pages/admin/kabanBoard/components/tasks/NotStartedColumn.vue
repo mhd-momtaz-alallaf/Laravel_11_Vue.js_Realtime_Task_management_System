7ddfb3ff16c6f3532d6e5a4af13b674cb8d555aa
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-30 23:55:49 +0300

    Modifying the NotStartedColumn.vue component to list all the Not Started Tasks with its members

--- resources/js/src/pages/admin/kabanBoard/components/tasks/NotStartedColumn.vue
+++ resources/js/src/pages/admin/kabanBoard/components/tasks/NotStartedColumn.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import { getFirstChar } from '../../../../../helpers/util';
+import { SingleProjectResponseType, TaskStatus } from '../../actions/getProjectDetail.types';
+
+defineProps<{
+    projectData: SingleProjectResponseType;
+}>();
+
 const emit = defineEmits<{
     (e:'openTaskModal'): Promise<void>;
 }>();
@@ -10,25 +18,28 @@ const emit = defineEmits<{
             <button @click="emit('openTaskModal')" class="btn btn-warning">Add Task</button>
         </div>
 
-        <div class="card card-body task_card" draggable="true">
-            <p>Task Name</p>
-
-            <div class="assignees">
-                <button class="btn btn-primary member_1">I</button>
-                <button class="btn btn-light member_2">J</button>
-                <button class="btn btn-secondary member_3">K</button>
-                3 Assignees
-            </div>
-        </div>
-
-        <div class="card card-body task_card" draggable="true">
-            <p>Task Name</p>
-
-            <div class="assignees">
-                <button class="btn btn-primary member_1">I</button>
-                <button class="btn btn-light member_2">J</button>
-                <button class="btn btn-secondary member_3">K</button>
-                3 Assignees
+        <div class="scrollable-tasks">
+            <div
+                v-show="task.status === TaskStatus.NOT_STARTED ? true : false"
+                v-for="task in projectData?.data?.tasks"
+                :key="task.id"
+                draggable="true"
+                :class="'card card-body task_card notStartedTask_'+task.id"
+            >
+                <p>{{task.name}}</p>
+                <div class="assignees">
+                    <div class="assignees-buttons">
+                        <button v-for="(member, index) in task.task_members.slice(0, 4)"
+                            :key="member.id"
+                            :class="`btn btn-primary member_${index}`"
+                        >
+                            {{ getFirstChar(member?.members?.name) }}
+                        </button>
+                    </div>
+                    <div class="assignees-count">
+                        {{ task?.task_members.length }} assignees
+                    </div>
+                </div>
             </div>
         </div>
     </div>
