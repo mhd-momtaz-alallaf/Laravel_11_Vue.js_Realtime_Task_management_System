34eabb5ad02b25a1c069ac6442b2bddb2a0487d6
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-07-30 12:17:22 +0300

    Creating the BreadCrumb.vue, ProjectData.vue and ProjectProgress.vue Components to use it in the KabanBoard.vue main page

---
+++ resources/js/src/pages/admin/kabanBoard/components/ProjectData.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="row" style="margin-bottom: 20px;">
+        <h3 style="color: #767778;">
+            Project: {{ ProjectDetail?.data?.name }}
+        </h3>
+        <span>
+            Starting Date: <b>{{ ProjectDetail?.data?.startDate }}</b> - Ending Date: <b>{{ ProjectDetail?.data?.endDate }}</b>
+        </span>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { SingleProjectResponseType } from "../actions/getProjectDetail.types"
+
+defineProps<{
+    ProjectDetail: SingleProjectResponseType;
+}>();
+</script>
---
+++ resources/js/src/pages/admin/kabanBoard/components/ProjectProgress.vue
@@ -0,0 +1,30 @@
+<!-- a Progress Bar from Bootstrap Documentation -->
+<template>
+    <div class="row">
+        <div class="col-md-12">
+            <div
+                class="progress"
+                role="progressbar"
+                aria-label="Example with label"
+                aria-valuenow="5"
+                aria-valuemin="0"
+                aria-valuemax="100"
+            >
+                <div
+                    class="progress-bar bg-success"
+                    :style="{width:parseInt(ProjectDetail?.data?.task_progress?.progress)+'%'}"
+                >
+                    {{ ProjectDetail?.data?.task_progress?.progress }} %
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { SingleProjectResponseType } from "../actions/getProjectDetail.types"
+
+defineProps<{
+    ProjectDetail:SingleProjectResponseType
+}>()
+</script>
