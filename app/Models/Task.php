4cfae14993264158d937a7b94fcea17b0db5dc8b
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-08-01 21:17:50 +0300

    Creating the handleProjectProgress static function in the Task.php model file

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -32,4 +32,59 @@ class Task extends Model
         Task::where('id', $taskId)
             ->update(['status' => $status]);
     }
+
+    public static function countCompletedTasks($projectId)
+    {
+        $count = Task::where('project_id', $projectId)
+            ->where('status', Task::COMPLETED)
+            ->count();
+
+        return $count;
+    }
+
+    public static function countCompletedAndPendingTasks($projectId)
+    {
+        $taskCounts = Task::where('project_id', $projectId)
+            ->selectRaw('SUM(status = ?) as pending, SUM(status = ?) as completed', [Task::PENDING, Task::COMPLETED])
+            ->first();
+
+        $pending = (int) $taskCounts->pending ?? 0;
+        $completed = (int) $taskCounts->completed ?? 0;
+
+        return [$pending, $completed];
+    }
+
+    public static  function countProjectTasks($projectId)
+    {
+        $count = Task::where('project_id', $projectId)->count();
+
+        return $count;
+    }
+
+    public static function aroundNumber($number)
+    {
+        if (strpos($number, '.')) {
+            $position = strpos($number, '.') + 1;
+            return substr($number, 0, $position + 1);
+        } else {
+            return $number;
+        }
+    }
+
+    public static function handleProjectProgress($projectId)
+    {
+        $totalTasks = Task::countProjectTasks($projectId);
+        $totalCompletedTasks = Task::countCompletedTasks($projectId);
+
+        $progress = Task::aroundNumber(($totalCompletedTasks * 100) / $totalTasks);
+        $taskProgress = TaskProgress::where('project_id', $projectId)->first();
+
+        if (!is_null($taskProgress)) {
+
+            $taskProgress->where('project_id', $projectId)
+                ->update(['progress' => $progress]);
+
+            return $progress;
+        }
+    }
 }
