57d45291fad54d414fb65fb146ab043e6bdc3c12
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-08-03 21:39:54 +0300

    Listening on the TrackingProjectTasksCount Event in the getChartsData.ts helper action to get the tasks status chart data in RealTime on the DashboardPage.vue

--- resources/js/src/pages/admin/dashboard/actions/getChartsData.ts
+++ resources/js/src/pages/admin/dashboard/actions/getChartsData.ts
@@ -22,12 +22,19 @@ export function useGetChartData() {
         }
     };
 
-    // using laravel reverb to update the projects Progress in realtime with each time a task status of a Project is changed.
+    // using laravel reverb to update the projects Progress and tasks status count in realtime with each time a task status of a Project is changed.
     function updateData() {
         // listening to 'projectProgressChannel' of 'TrackingProjectProgress' Event.
         window.Echo.channel("projectProgressChannel").listen("TrackingProjectProgress", (e: {projectProgress: number}) => {
             chartData.value.progress = 0; // initializing the progress.
             setTimeout(() => chartData.value.progress = e.projectProgress, 300);
+            // console.log(e);
+            }
+        );
+
+        window.Echo.channel("tasksCountChannel").listen("TrackingProjectTasksCount", (e: {tasks: Array<number>}) => {
+            chartData.value.tasks = undefined as any; // initializing the tasks.
+            setTimeout(() => chartData.value.tasks = e.tasks, 200);
             // console.log(e);
             }
         );
